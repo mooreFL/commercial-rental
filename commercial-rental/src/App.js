b090edc88cccd0341bad55d938a9544a76b82c26
mooreFL <moorepr09@gmail.com>
2021-10-25 22:18:03 -0400

    initial commit

---
+++ commercial-rental/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components.js/Navbar"
+import './App.css';
+
+export default function App() {
+  return (
+   <main>
+   <Navbar />
+   </main>
+  );
+}
